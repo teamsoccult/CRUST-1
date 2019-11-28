d84850506f3a15395eaf3f6bf397b31ee81fe6c4
Victor Møller Poulsen <victormoeller@gmail.com>
2019-11-28 11:20:07 +0100

    comment

---
+++ DevezerAlteredAgents/src/functions/modelSimilarByInteractionRework.R
@@ -0,0 +1,94 @@
+################
+## Extremely crude and only to be used as a backup, but it works. Makes Bo great again. 
+## Changed: 24/11/2019
+################
+
+modelSimilarByInteraction <- function(model, models){
+
+if(identical(model, models[[1]])){
+  number <- sample(5:6, 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[2]])){
+  number <- 5
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[3]])){
+  number <- 6
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[4]])){
+  number <- sample(7:9, 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[5]])){
+  number <- 2
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[6]])){
+  number <- 3
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[7]])){
+  number <- sample(c(4,10,11), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[8]])){
+  number <- sample(c(4,10,12), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[9]])){
+  number <- sample(c(4,11,12), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[10]])){
+  number <- sample(c(7,8,13), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[11]])){
+  number <- sample(c(7,9,13), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[12]])){
+  number <- sample(c(8,9,13), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[13]])){
+  number <- sample(c(10, 11, 12, 14), 1)
+  
+  similarModel <- models[[number]]
+}
+
+else if(identical(model, models[[14]])){
+  number <- 13
+  
+  similarModel <- models[[number]]
+}
+  
+return(similarModel)
+  
+}
